a84baf4901cc7a69cb795646cad4a458c19e68f2
LubovLepekhina <lubov.lepekhina@gmail.com>
2025-08-14 18:51:27 +0300

    solved hw-8

--- homework-solutions/js/hw-8/task-3.mjs
+++ homework-solutions/js/hw-8/task-3.mjs
@@ -7,7 +7,23 @@
 */
 
 function findMissingNumber(numbers) {
-  // Ваш код
+  const sortedUniq = [...new Set(numbers.sort((a, b) => a - b))];
+  let result = 0;
+  if (sortedUniq[0] !== 1) {
+    result = 1;
+  } else {
+    for (let i = 0; i < sortedUniq.length - 1; i++) {
+      if (sortedUniq[i + 1] - sortedUniq[i] > 1) {
+        result = sortedUniq[i] + 1;
+      }
+    }
+  }
+  if (result === 0) {
+    result = sortedUniq[sortedUniq.length - 1] + 1;
+  }
+  
+  return result;
 }
+console.log(findMissingNumber([5,2,7,3,8,1,6]));
 
 export { findMissingNumber };
